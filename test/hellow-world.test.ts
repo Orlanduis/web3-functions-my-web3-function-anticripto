f55e0e423fc2b879b62b904104b5321d1bde0a7c
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-10-09 05:23:24 -0600

    Update hellow-world.test.ts

--- test/hellow-world.test.ts
+++ test/hellow-world.test.ts
@@ -1,8 +1,18 @@
-  import { AnvilServer } from "https://drpc.org/dashboard/909f16c3-fe5c-41b5-a1cb-e887c44d3d8d/keys/0601ff3b-72a9-11ef-8ebf-8650d87f0e2a";
+  import { DrpcServer } from "https://drpc.org/dashboard/909f16c3-fe5c-41b5-a1cb-e887c44d3d8d/keys/0601ff3b-72a9-11ef-8ebf-8650d87f0e2a";
 
   Drpc fork = await Drcpserver.fork({
     forkBlockNumber: latest,
     forkUrl: "https://lb.drpc.org/ogrpc?network=optimism&dkey=ApCfFsP-XEG1ocvoh8RNPY0GAf87cqkR746_hlDYfw4q",
   });
 
-  const forkedProvider = DrpcFork.provider;
+  const forkedProvider = DrpcFork.provider,
+    }
+}
+    import { buildbearServer } from "https://rpc.buildbear.io/esquivelfabian
+
+  goerliFork = await AnvilServer.fork({
+    forkBlockNumber: 8483100,
+    forkUrl: "https://rpc.buildbear.io/esquivelfabian",
+  });
+
+  const forkedProvider = buildbearFork.provider;
