5cbf4daa2a955e320a560b08ae70f5c7cb9a93fa
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-10-09 05:19:25 -0600

    Create hellow-world.test.ts

---
+++ test/hellow-world.test.ts
@@ -0,0 +1,8 @@
+  import { AnvilServer } from "https://drpc.org/dashboard/909f16c3-fe5c-41b5-a1cb-e887c44d3d8d/keys/0601ff3b-72a9-11ef-8ebf-8650d87f0e2a";
+
+  Drpc fork = await Drcpserver.fork({
+    forkBlockNumber: latest,
+    forkUrl: "https://lb.drpc.org/ogrpc?network=optimism&dkey=ApCfFsP-XEG1ocvoh8RNPY0GAf87cqkR746_hlDYfw4q",
+  });
+
+  const forkedProvider = DrpcFork.provider;
